f9843d60da79e993e0df684bd23e3ea5cb04bc19
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-09-27 15:46:35 +0300

    init: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,34 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
+import prettierConfig from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-    {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-    {ignores: ['stubs/**.*', 'stubs/**/**.*', 'bro.config.js', 'src/index.tsx']},
-    {languageOptions: {globals: globals.browser}},
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    {
+        ignores: [
+            'stubs/**.*',
+            'stubs/**/**.*',
+            'bro.config.js',
+            'src/index.tsx',
+        ],
+    },
+    { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     pluginReact.configs.flat.recommended,
+    {
+        plugins: {
+            prettier: pluginPrettier,
+        },
+    },
+    {
+        rules: {
+            ...prettierConfig.rules,
+            'prettier/prettier': 'error',
+        },
+    },
 ];
