dbd4afe900d42d04fb7a488f325c75a8c9d7d8af
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-09-27 16:06:02 +0300

    init: add lint-staged

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
 
 const App = () => {
-    return <h1>Hello world для проекта -
-
-
-
-
-
-
-
-
-
-        portfolio_app</h1>;
+    return <h1>Hello world для проекта - portfolio_app</h1>;
 };
 
 export default App;
