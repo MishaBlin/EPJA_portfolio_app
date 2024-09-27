6c5981ac68d2595246a84359184f31534e984267
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-09-27 15:48:45 +0300

    init: add husky

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const App = () => {
-    return <h1>Hello world для проекта - portfolio_app</h1>;
+    return <h1>Hello world для проекта -
+
+
+
+
+
+
+
+
+
+        portfolio_app</h1>;
 };
 
 export default App;
