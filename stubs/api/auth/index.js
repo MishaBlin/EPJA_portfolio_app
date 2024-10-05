27c1c478582db6989cd3995cafec905587f4977c
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-10-05 14:40:19 +0300

    feat: add api to get page info

--- stubs/api/auth/index.js
+++ stubs/api/auth/index.js
@@ -8,8 +8,8 @@ authRouter.post('/login', (req, res) => {
     console.log(`Login with email=${email} and password=${password}`);
 
     if (email === 'admin@admin.admin' && password === 'admin') {
-        res.status(200).send({ 'status': 'OK', 'token': `Token ${process.env.TOKEN}` });
+        res.status(200).send({ 'status': 'OK', 'data': `${process.env.TOKEN}` });
     } else {
-        res.status(401).send({ message: 'Invalid email or password' });
+        res.status(401).send({ 'status': 'Failed!', 'data': 'Invalid email or password' });
     }
 });
--- stubs/api/root/index.js
+++ stubs/api/root/index.js
@@ -1,4 +1,30 @@
 const rootRouter = require('express').Router();
 require('dotenv').config();
+const data = require('../data.json');
+
+rootRouter.get('/get/nickname', (req, res) => {
+    res.status(200).send({ 'status': 'OK', 'data': { 'nickname': data.nickname } });
+});
+
+rootRouter.get('/get/tech-stack', (req, res) => {
+    res.status(200).send({ 'status': 'OK', 'data': { 'techStack': data.techStack } });
+});
+
+rootRouter.get('/get/github-repo', (req, res) => {
+    res.status(200).send({ 'status': 'OK', 'data': { 'githubRepo': data.githubRepo } });
+});
+
+rootRouter.get('/get/city', (req, res) => {
+    res.status(200).send({ 'status': 'OK', 'data': { 'city': data.city } });
+});
+
+rootRouter.get('/get/nav-links', (req, res) => {
+    res.status(200).send({ 'status': 'OK', 'data': { 'city': data.navLinks } });
+});
+
+rootRouter.get('/get/links', (req, res) => {
+    res.status(200).send({ 'status': 'OK', 'data': { 'city': data.links } });
+});
+
 
 module.exports = rootRouter;
