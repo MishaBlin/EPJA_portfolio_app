20e870a845797f09275d14e9cc56b7a4b22ab378
Mikhail <mikhail_kalinin_a@mail.ru>
2024-10-05 21:58:47 +0300

    More styles

--- src/components/router/admin/(admin)/page.tsx
+++ src/components/router/admin/(admin)/page.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
-import AboutEdit from './about-edit';
+import Connect from '../../root/(about)/connect';
+import Stars from '../../root/(about)/stars';
+import Location from '../../root/(about)/location';
+import TechStack from '../../root/(about)/tech-stack';
+
+import EditCity from './edit-city';
+import EditGitHub from './edit-github';
+import EditTechStack from './edit-tech-stack';
+import EditSocials from './edit-socials';
+import Nickname from './edit-nickname';
 
 export default function AdminPage() {
-    return <AboutEdit />;
+    return (
+        <div id="about">
+            <h1 className="text-4xl font-bold mb-4">Admin panel</h1>
+            <div className="w-full flex gap-x-4 items-stretch">
+                {/*Левый блок*/}
+                <div className="w-1/2 flex flex-col gap-y-4">
+                    {/*Связь со мной*/}
+                    <Connect editButton={<EditSocials />} />
+
+                    {/*Звёзды*/}
+                    <Stars editButton={<EditGitHub />} />
+                </div>
+
+                {/*Правый блок*/}
+                <div className="w-1/2 flex flex-col gap-y-4">
+                    <div className="flex gap-x-4">
+                        <Nickname />
+
+                        {/*Локация*/}
+                        <Location editButton={<EditCity />} />
+                    </div>
+
+                    {/*Стек*/}
+                    <TechStack editButton={<EditTechStack />} />
+                </div>
+            </div>
+        </div>
+    );
 }
