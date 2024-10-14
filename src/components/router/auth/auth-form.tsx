ae9be59854ca0f381a7e4f5bf4470088932be8e7
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-10-14 14:10:25 +0500

    feat: update backend url

--- src/components/router/auth/auth-form.tsx
+++ src/components/router/auth/auth-form.tsx
@@ -29,7 +29,7 @@ export function UserAuthForm({ className, ...props }: AuthProps) {
 
         setTimeout(async () => {
             await axios
-                .post('/api/cats/auth/login', {
+                .post('/ms/epja-2024-1/cats/auth//login', {
                     email: email,
                     password: password,
                 })
